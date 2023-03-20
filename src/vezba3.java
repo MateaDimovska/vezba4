4cc8615b2643568a64dcfb38d7d52aa226b45af6
unknown <matea123dimovska@gmail.com>
2023-03-20 12:03:08 +0100

    Add files to repo

---
+++ src/vezba3.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class vezba3 {
+
+public static void main(String[] args) {
+
+int k;
+
+Scanner tastatura = new Scanner(System.in);
+
+k = tastatura.nextInt();
+
+if(k>6) {
+
+System.out.println(k);
+
+}
+
+else {
+
+System.out.println("Vrednosta na K e pomala od 6.");
+
+}
+
+}
+
+}
